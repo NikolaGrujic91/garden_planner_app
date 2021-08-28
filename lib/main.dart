036dcc044bcef00a2896de7e9bde952151d14335
Nikola Grujić <grujic.nikola91@gmail.com>
2021-08-28 21:05:26 +0200

    Linter: main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:garden_planner_app/model/gardens_store.dart';
+import 'package:garden_planner_app/screens/add_garden_screen.dart';
+import 'package:garden_planner_app/screens/edit_garden_screen.dart';
+import 'package:garden_planner_app/screens/edit_tile_plants_screen.dart';
+import 'package:garden_planner_app/screens/edit_tile_type_screen.dart';
+import 'package:garden_planner_app/screens/main_screen.dart';
+import 'package:garden_planner_app/screens/tiles_screen.dart';
 import 'package:provider/provider.dart';
-import 'model/gardens_store.dart';
-import 'screens/main_screen.dart';
-import 'screens/add_garden_screen.dart';
-import 'screens/edit_garden_screen.dart';
-import 'screens/tiles_screen.dart';
-import 'screens/edit_tile_type_screen.dart';
-import 'screens/edit_tile_plants_screen.dart';
 
 void main() {
-  runApp(GardenPlannerApp());
+  runApp(const GardenPlannerApp());
 }
 
+/// This widget is the root of the application.
 class GardenPlannerApp extends StatelessWidget {
-  // This widget is the root of your application.
+  /// Creates a new instance
+  const GardenPlannerApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider<GardensStore>(
