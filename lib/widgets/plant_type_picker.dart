01d6bdc412e17bc9518d8ef3c09e66d5c02de096
Nikola Grujić <grujic.nikola91@gmail.com>
2021-08-29 11:16:43 +0200

    Linter: plant_type_picker.dart

--- lib/widgets/plant_type_picker.dart
+++ lib/widgets/plant_type_picker.dart
@@ -1,16 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_material_pickers/flutter_material_pickers.dart';
+import 'package:garden_planner_app/utils/constants.dart';
+import 'package:garden_planner_app/utils/utility.dart';
 
-import '../utils/constants.dart';
-import '../utils/utility.dart';
-
+/// This widget enables picking plant type via modal dialog
 class PlantTypePicker extends StatelessWidget {
-  final List<String> dropdownValues;
-  final String value;
-  final int index;
-  final Function callback;
+  /// Creates a new instance
+  const PlantTypePicker({
+    Key? key,
+    required this.dropdownValues,
+    required this.value,
+    required this.index,
+    required this.callback,
+  }) : super(key: key);
 
-  const PlantTypePicker({Key? key, required this.dropdownValues, required this.value, required this.index, required this.callback}) : super(key: key);
+  /// List of values to display in the picker
+  final List<String> dropdownValues;
+
+  /// Value of initially selected value
+  final String value;
+
+  /// Index of selected tile
+  final int index;
+
+  /// Callback function
+  final Function(String plantType, int index) callback;
 
   @override
   Widget build(BuildContext context) {
@@ -23,7 +37,7 @@ class PlantTypePicker extends StatelessWidget {
   }
 
   Future<void> _showPlantTypePicker(BuildContext context, int index) async {
-    showMaterialRadioPicker<String>(
+    await showMaterialRadioPicker<String>(
       context: context,
       headerColor: kAppBarBackgroundColor,
       title: 'Pick plant type',
