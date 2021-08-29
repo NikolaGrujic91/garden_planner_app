e50cf397390078c0c1aab0079a961f406ddd2e61
Nikola Grujić <grujic.nikola91@gmail.com>
2021-08-29 10:41:48 +0200

    Linter: styled_text.dart

--- lib/widgets/styled_text.dart
+++ lib/widgets/styled_text.dart
@@ -1,16 +1,19 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:garden_planner_app/utils/constants.dart';
 
-import '../utils/constants.dart';
-
+/// This widget represents styled text widget
 class StyledText extends StatelessWidget {
-  final String text;
+  /// Creates a new instance
   const StyledText({Key? key, required this.text}) : super(key: key);
+
+  /// Text
+  final String text;
 
   @override
   Widget build(BuildContext context) {
     return Text(
-      this.text,
+      text,
       style: kTextStyle,
     );
   }
