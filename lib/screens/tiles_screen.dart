92d4e7f6b24fa790c6aada4369ca1a25801e6194
Nikola Grujić <grujic.nikola91@gmail.com>
2021-08-29 15:52:53 +0200

    Linter: tiles_screen.dart

--- lib/screens/tiles_screen.dart
+++ lib/screens/tiles_screen.dart
@@ -1,13 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:garden_planner_app/model/gardens_store.dart';
+import 'package:garden_planner_app/screens/main_screen.dart';
+import 'package:garden_planner_app/widgets/base_app_bar.dart';
+import 'package:garden_planner_app/widgets/tiles_bottom_bar.dart';
+import 'package:garden_planner_app/widgets/tiles_grid_view.dart';
 import 'package:provider/provider.dart';
 
-import '../model/gardens_store.dart';
-import '../screens/main_screen.dart';
-import '../widgets/base_app_bar.dart';
-import '../widgets/tiles_bottom_bar.dart';
-import '../widgets/tiles_grid_view.dart';
-
+/// Tiles Screen Widget
 class TilesScreen extends StatelessWidget {
+  /// Creates a new instance
+  const TilesScreen({Key? key}) : super(key: key);
+
+  /// Tiles Screen ID
   static const String id = 'tiles_screen';
 
   @override
@@ -18,10 +22,10 @@ class TilesScreen extends StatelessWidget {
           backScreenID: MainScreen.id,
           title: gardensStore.gardens[gardensStore.selectedGardenIndex].name,
         ),
-        body: SafeArea(
+        body: const SafeArea(
           child: TilesGrid(),
         ),
-        bottomNavigationBar: TilesBottomBar(),
+        bottomNavigationBar: const TilesBottomBar(),
       );
     });
   }
