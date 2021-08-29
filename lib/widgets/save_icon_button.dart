5dc9e13b623a0141173eae8bd173277dd08e0ae7
Nikola Grujić <grujic.nikola91@gmail.com>
2021-08-29 10:45:19 +0200

    Linter: save_icon_button.dart

--- lib/widgets/save_icon_button.dart
+++ lib/widgets/save_icon_button.dart
@@ -1,15 +1,18 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
+/// This widget represents reusable save button
 class SaveIconButton extends StatelessWidget {
-  SaveIconButton({required this.callback});
+  /// Creates a new instance
+  const SaveIconButton({required this.callback});
 
+  /// Callback function
   final VoidCallback callback;
 
   @override
   Widget build(BuildContext context) {
     return IconButton(
-      onPressed: this.callback,
+      onPressed: callback,
       icon: const Icon(Icons.save),
       tooltip: 'Save',
     );
--- lib/widgets/styled_text.dart
+++ lib/widgets/styled_text.dart
@@ -2,7 +2,7 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:garden_planner_app/utils/constants.dart';
 
-/// This widget represents styled text widget
+/// This widget represents reusable styled text widget
 class StyledText extends StatelessWidget {
   /// Creates a new instance
   const StyledText({Key? key, required this.text}) : super(key: key);
