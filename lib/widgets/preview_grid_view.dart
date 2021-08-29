7ce20f8ff277cb9fae98c88284667c493ac467e5
Nikola Grujić <grujic.nikola91@gmail.com>
2021-08-29 10:53:28 +0200

    Linter: preview_grid_view.dart

--- lib/widgets/preview_grid_view.dart
+++ lib/widgets/preview_grid_view.dart
@@ -1,27 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:garden_planner_app/utils/constants.dart';
 
+/// This widget presents a preview of tiles grid while creating a garden
 class PreviewGridView extends StatelessWidget {
+  /// Creates a new instance
   const PreviewGridView({required this.columns, required this.rows});
 
+  /// Number of columns
   final int columns;
+
+  /// Number of rows
   final int rows;
 
   @override
   Widget build(BuildContext context) {
-    // Calculate aspect ratio in order to make all grid cells always visible properly
-    var size = MediaQuery.of(context).size;
-    var aspectRatio = (size.width / columns) / ((size.height - (53 * 6)) / rows);
+    // Calculate aspect ratio in order to make
+    // all grid cells always visible properly
+    final size = MediaQuery.of(context).size;
+    final aspectRatio =
+        (size.width / columns) / ((size.height - (53 * 6)) / rows);
 
     return Expanded(
       child: GridView.count(
         crossAxisCount: columns,
         childAspectRatio: aspectRatio,
         children: List.generate(columns * rows, (index) {
-          return Padding(
-            padding: const EdgeInsets.all(0.5),
+          return const Padding(
+            padding: EdgeInsets.all(0.5),
             child: Material(
               child: ListTile(
-                tileColor: Color.fromARGB(255, 146, 168, 144),
+                tileColor: kTilePlantColor,
               ),
             ),
           );
