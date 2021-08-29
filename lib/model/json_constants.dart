1d6493678f3300e743826e1fbf39f93d236a4740
Nikola Grujić <grujic.nikola91@gmail.com>
2021-08-29 17:04:17 +0200

    Linter: json_constants.dart

--- lib/model/json_constants.dart
+++ lib/model/json_constants.dart
@@ -1,11 +1,32 @@
+/// JSON Type constant
 const String kJsonType = 'type';
+
+/// JSON Plants constant
 const String kJsonPlants = 'plants';
+
+/// JSON Plant Name constant
 const String kJsonPlantName = 'plantName';
+
+/// JSON Planted Date constant
 const String kJsonPlantedDate = 'plantedDate';
+
+/// JSON Description constant
 const String kJsonDescription = 'description';
+
+/// JSON Name constant
 const String kJsonName = 'name';
+
+/// JSON Rows constant
 const String kJsonRows = 'rows';
+
+/// JSON Columns constant
 const String kJsonColumns = 'columns';
+
+/// JSON Tiles constant
 const String kJsonTiles = 'tiles';
+
+/// JSON Gardens constant
 const String kJsonGardens = 'gardens';
+
+/// JSON File Name constant
 const String kJsonFileName = 'gardens.json';
