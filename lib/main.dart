fb3d437e6e2242f9969408e8a4f2fb11ee438852
Nikola Grujić <grujic.nikola91@gmail.com>
2021-09-08 07:28:16 +0200

    Added take_picture_screen.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+import 'dart:io' show Platform;
+
+import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:garden_planner_app/db/gardens_store_hive.dart';
 import 'package:garden_planner_app/screens/add_garden_screen.dart';
@@ -5,17 +8,35 @@ import 'package:garden_planner_app/screens/edit_garden_screen.dart';
 import 'package:garden_planner_app/screens/edit_tile_plants_screen.dart';
 import 'package:garden_planner_app/screens/edit_tile_type_screen.dart';
 import 'package:garden_planner_app/screens/main_screen.dart';
+import 'package:garden_planner_app/screens/take_picture_screen.dart';
 import 'package:garden_planner_app/screens/tiles_screen.dart';
 import 'package:provider/provider.dart';
 
-void main() {
-  runApp(const GardenPlannerApp());
+Future<void> main() async {
+  if (Platform.isAndroid || Platform.isIOS) {
+    // Ensure that plugin services are initialized so that `availableCameras()`
+    // can be called before `runApp()`
+    WidgetsFlutterBinding.ensureInitialized();
+
+    // Obtain a list of the available cameras on the device.
+    final cameras = await availableCameras();
+
+    runApp(GardenPlannerApp(camera: cameras.first));
+  } else {
+    runApp(const GardenPlannerApp(camera: null));
+  }
 }
 
 /// This widget is the root of the application.
 class GardenPlannerApp extends StatelessWidget {
   /// Creates a new instance
-  const GardenPlannerApp({Key? key}) : super(key: key);
+  const GardenPlannerApp({
+    Key? key,
+    required this.camera,
+  }) : super(key: key);
+
+  /// Camera
+  final CameraDescription? camera;
 
   @override
   Widget build(BuildContext context) {
@@ -31,6 +52,7 @@ class GardenPlannerApp extends StatelessWidget {
           TilesScreen.id: (context) => const TilesScreen(),
           EditTileTypeScreen.id: (context) => const EditTileTypeScreen(),
           EditTilePlantsScreen.id: (context) => const EditTilePlantsScreen(),
+          TakePictureScreen.id: (context) => TakePictureScreen(camera: camera!)
         },
       ),
     );
