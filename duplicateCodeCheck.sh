ebebcda35d605931c69bb817f3250187ad96b3f5
Nikola Grujić <grujic.nikola91@gmail.com>
2021-12-07 06:40:14 +0100

    Create duplicateCodeCheck.sh

---
+++ duplicateCodeCheck.sh
@@ -0,0 +1,11 @@
+# https://github.com/kucherenko/jscpd
+#The jscpd gives the ability to find duplicated blocks implemented on more than
+# 150 programming languages and digital formats of documents.
+
+# chmod +x ./duplicateCodeCheck.sh
+
+#install
+#npm install -g jscpd
+
+#usage
+npx jscpd --reporters "consoleFull" --format "dart" lib
